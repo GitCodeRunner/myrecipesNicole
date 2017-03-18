003b38c29080eb65ce2240d7a74fb9b96c35500b
recode now <recodenow@gmail.com>
2017-03-18 17:14:35 +0000

    add listing and pagination

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,10 @@
 class ChefsController < ApplicationController
   before_action :set_chef, only: [:show, :edit, :update]
   
+  def index
+    @chefs = Chef.paginate(page: params[:page], per_page: 5)
+  end
+  
   def new
     @chef = Chef.new
   end
@@ -16,7 +20,7 @@ class ChefsController < ApplicationController
   end
   
   def show
-    
+    @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 5)
   end
   
   def edit
